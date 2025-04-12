6dd38636ce2d2e04e82f2ab1119fe09a241af096
Yogesh Ghadge <99175443+escape2005@users.noreply.github.com>
2025-04-12 13:27:41 +0530

    Notifications added

--- plant_care_app/lib/main.dart
+++ plant_care_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:plant_care_app/pages/bottom_nav.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'pages/login-signup/landing_page.dart';
@@ -8,20 +9,35 @@ import 'pages/login-signup/forgot_password.dart';
 import 'package:plant_care_app/pages/provider/theme_provider.dart';
 import 'package:provider/provider.dart';
 
+/// Global instance of the local notifications plugin
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized(); // Ensure Flutter bindings are initialized
 
+  // Initialize Supabase
   await Supabase.initialize(
     url: 'https://xbohbkzamxgocrpyzydf.supabase.co',
-    anonKey:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhib2hia3phbXhnb2NycHl6eWRmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI3NDY4MDMsImV4cCI6MjA1ODMyMjgwM30.IntPbBWFhBc63lRNidOymoj3iazHGMa5lYSMNo68JRQ', // Replace with your actual anon key
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhib2hia3phbXhnb2NycHl6eWRmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI3NDY4MDMsImV4cCI6MjA1ODMyMjgwM30.IntPbBWFhBc63lRNidOymoj3iazHGMa5lYSMNo68JRQ',
   );
 
+  // Initialize local notifications
+  const AndroidInitializationSettings initializationSettingsAndroid =
+      AndroidInitializationSettings('@mipmap/ic_launcher');
+  //final IOSInitializationSettings initializationSettingsIOS =
+      //IOSInitializationSettings();
+  final InitializationSettings initializationSettings = InitializationSettings(
+    android: initializationSettingsAndroid,
+    //iOS: initializationSettingsIOS,
+  );
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+
+  // Run app
   runApp(
     ChangeNotifierProvider(
-      create:(context) => ThemeProvider(),
-      child:const MyApp(),
-    ) as Widget,
+      create: (context) => ThemeProvider(),
+      child: const MyApp(),
+    ),
   );
 }
 
@@ -34,15 +50,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Plantify',
-<<<<<<< Updated upstream
       theme: ThemeData(primarySwatch: Colors.green),
       initialRoute: '/welcome',
-=======
-      theme:ThemeProvider.lightTheme,
-      darkTheme: ThemeProvider.darkTheme,
-      themeMode: themeProvider.themeMode,
-      initialRoute: '/home',
->>>>>>> Stashed changes
       routes: {
         '/welcome': (context) => const WelcomeScreen(),
         '/login': (context) => const LoginScreen(),
